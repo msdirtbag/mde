394fb06350933bb8c16079e4990216634631ca75
msdirtbag <105983492+msdirtbag@users.noreply.github.com>
2022-10-21 14:20:06 -0500

    Add files via upload

---
+++ enableMDEnetworkdetection.ps1
@@ -0,0 +1,13 @@
+﻿# =================================================================================================#
+#                                 Enable MDE Network Detection on Windows                          #
+# =================================================================================================#
+
+Set-MpPreference -EnableNetworkProtection Enabled
+Set-MpPreference -AllowNetworkProtectionOnWinServer 1
+Set-MpPreference -AllowNetworkProtectionDownLevel 1
+Set-MpPreference -AllowDatagramProcessingOnWinServer 1
+Auditpol /set /category:”System” /SubCategory:”Filtering Platform Packet Drop” /success:enable /failure:enable
+Auditpol /set /category:”System” /SubCategory:”Filtering Platform Connection” /success:enable /failure:enable
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+
+exit
